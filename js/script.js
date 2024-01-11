017d3b32b1391a40e6ce3836e7f82cd13586caaa
selly <yppeu@naver.com>
2024-01-11 15:44:36 +0900

    0111

--- js/script.js
+++ js/script.js
@@ -1,43 +1,32 @@
 $(document).ready(function () {
-  let basicFont = 12; // 기본 폰트 크기를 12px로 지정
-
   //  스크롤 맨 위로
   $("#btn-gotop").click(function () {
     $("html, body").animate({ scrollTop: 0 }, "300");
   });
 
-  // 텍스트 축소 및 확대
+  let basicFont = 14; // 기본 폰트 크기를 12px로 지정
+
+  // 텍스트확대
   $(".btn-zoomIn").click(function () {
-    // zoomIn을 누르면, 함수 발생
-    basicFont++; // basicFont는 1씩 계속 증가하고,
+    console.log(1);
+    basicFont++;
 
     // 20 넘어가면 20에서 동작 멈춤
-    if (basicFont > 20) {
-      basicFont = 20;
+    if (basicFont > 15) {
+      basicFont = 15;
       return false;
     }
-
-    // 기존에 크기가 몇인지 알려주던 p 태그의 텍스트도 현재 폰트 크기랑 같이 변경
-    $(".fontSize").text(basicFont + "px");
-    // txtBox의 폰트 크기는, 변수 basicFont 크기대로 감
-    $("#txtBox").css("font-size", basicFont);
+    $(".fontSize").css("font-size", basicFont + "px"); // 폰트 크기는, 변수 basicFont 크기대로 감
   });
-
-  $(".zoomOut").click(function () {
-    // zoomOut을 누르면, 함수 발생
+  // 텍스트 축소
+  $(".btn-zoomOut").click(function () {
     basicFont--; // basicFont가 1씩 계속 감소
 
     // 10 이하로 내려가면 10에서 동작 멈춤
-    if (basicFont < 10) {
-      basicFont = 10;
+    if (basicFont < 13) {
+      basicFont = 13;
       return false;
     }
-    $(".fontSize").text(basicFont + "px");
-    $("#txtBox").css("font-size", basicFont);
-  });
-
-  $(".origin").click(function () {
-    $(".fontSize").text("12px");
-    $("#txtBox").css("font-size", "12px");
+    $(".fontSize").css("font-size", basicFont + "px");
   });
 });
